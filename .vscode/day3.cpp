8f14c64a96d480cb794f9424aa669470a409e3cb
Mr-wolf-maaz <mrwolf@Mrwolf.localdomain>
2026-02-10 21:48:48 +0500

    day 3 practice

---
+++ .vscode/day3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+
+cout<<"This is day 3/n";
+
+
+
+
+    return 0;
+}
--- day1_hello.cpp
+++ day1_hello.cpp
@@ -4,7 +4,77 @@
 using namespace std;
 
 int main(){
-    cout<<"hello maaz"<<endl;
-    cout<<"first push "<<endl;
+//     cout<<"hello maaz"<<endl;
+//     cout<<"first push "<<endl;
+
+//     // This is Operater Program 
+    
+//     int a= 5;
+//     int b= 3;
+
+//     // Increment Decrement Operater 
+//     //       5    3     7     5     7     5     5      3  = 40
+//     int x = a++ + b++ + ++a + ++b + a-- + b-- + --a + --b;
+
+//     cout << x<<endl;
+
+//     // Binary Operater 
+
+//     int z = a+b;
+//     int r = a-b;
+//     int w = a*b;
+//     int t = a/b;
+
+//     cout << z <<endl;
+//     cout << r <<endl;
+//     cout << w <<endl;
+//     cout << t <<endl;
+
+//     // 
+
+//     cout<<"size of interger value "<<sizeof(int)<<endl;
+
+//     int c = 10<<2;
+//     int y = 10>>1;
+//     cout <<"this is c "<< c<<endl;
+//     cout <<"this is y "<< y<<endl;
+
+//    int no = 6;
+//    int sno = 10;
+
+//     cout<<"THIS IS AND OPERATER "<< (no & sno)<<"  THIS IS OR OPERATER "<< (no | sno)<<"  THIS IS NOT OPERATER "<< (no ^ sno)<<endl;
+    
+
+
+
+
+//    int number=32;
+
+//    if (number >> 2==2*2){
+//     cout<<"true";
+//    }
+//     else{
+//         cout <<"fals";
+//     }
+
+
+
+
+    int number =32;
+   
+
+    if (number <=0){
+        cout<<"false"<<endl;
+        while(number >1){
+
+            if(number%2 != 0){
+                cout<<"fasle"<<endl;
+                number/=2;
+            }
+            cout<<"true"<<endl;
+        }
+    }
+
+
     return 0;
 }
