8bcf901afddf3f71282f9f0992049de51c3154cf
Mr-wolf-maaz <saymaaz232@gmail.com>
2026-02-27 21:16:27 +0500

    do not practice code due to inter problem

---
+++ day18-recursion/day18.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+
+int arthimatic(int a, int b){
+     
+    int c =1, d=2;
+    arthimatic(int c, int d){
+        return a+b;
+    }
+
+
+
+
+}
+
+
+int main(){
+cout << "hello day 18\n";
+
+// today goal is to know recursion
+
+cout<< arthimatic(11,33)<<endl;
+
+
+    return 0;
+}
