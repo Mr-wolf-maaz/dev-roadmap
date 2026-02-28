089458c2847026fa0466393448f447771dffe6be
Mr-wolf-maaz <saymaaz232@gmail.com>
2026-02-28 22:24:48 +0500

    recursion in c++

--- day18-recursion/day18.cpp
+++ day18-recursion/day18.cpp
@@ -2,26 +2,45 @@
 using namespace std;
 
 
-int arthimatic(int a, int b){
-     
-    int c =1, d=2;
-    arthimatic(int c, int d){
-        return a+b;
+// Recursion to finde n to 1 number 
+void printnum(int n){
+      if(n==1){
+        cout << n<<"\n";
+        return;
+    }
+    cout<<n<<"\n";
+    printnum(n-1);
+  
+}
+
+// Recursion to finde n factorial 
+
+int factorial(int num){
+    if(num==0){
+      
+        return 1;
     }
 
-
-
+return num*factorial(num-1);
 
 }
 
+int sum(int n){
+    if (n==1){
+        return 1;
+    }
+    return n + sum(n-1);
+}
 
 int main(){
 cout << "hello day 18\n";
 
 // today goal is to know recursion
 
-cout<< arthimatic(11,33)<<endl;
 
+// printnum(n);
+// cout<<factorial(10)<<"\n";
+cout<<sum(2)<<endl;
 
     return 0;
 }
