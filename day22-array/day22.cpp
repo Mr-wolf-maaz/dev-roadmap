a519ea4de114f826f2bbc69d9c727c103232971e
Mr-wolf-maaz <saymaaz232@gmail.com>
2026-03-04 23:08:15 +0500

    complet array basic

--- day22-array/day22.cpp
+++ day22-array/day22.cpp
@@ -1,55 +1,101 @@
-#include <iostream>
-#include <climits>
-using namespace std;
+// #include <iostream>
+// #include <climits>
+// #include <utility>
+// using namespace std;
 
-int main(){
+// void reversArray(int rArray[], int size){
+//      int st =0;
+//      int end = size-1;
 
-    cout<<"Hello day 22 \n";
-    // int n=5;
-    // int marks[n];
+//      while(st<end){
+//      swap(rArray[st],rArray[end]);
+//         st ++; end--;
+//      }
 
-
-
-    // cout<<marks[0]<<"\n";
-    // cout<<marks[2]<<"\n";
-    // cout<<marks[3]<<"\n";
-    // cout<<marks[4]<<"\n";
-    // cout<<marks[1]<<"\n";
-   
-// for(int i=0; i<n; i++){
-//     cin>>marks[i];
 // }
 
-//     for(int i=0; i<n;i++){
-//         cout<<marks[i]<<"\n";
+// int main(){
+
+//     cout<<"Hello day 22 \n";
+// int size = 7;
+//     int uniqe[size]={1,2,3,1,2,3,4};
+     
+//     for(int i=0; i<size; i++){
+//         for(int j=i+1; j<size; j++){
+//             if(uniqe[j]!=uniqe[i]){
+//                 cout<<"uniqe number is :"<<uniqe[i]<<endl;
+//             }
+//         }
+//         cout<<endl;
 //     }
 
-int n = 5;
-int arr[n];
+
+
+
+    
+//     // int rArray[size]={1,2,3,4,5,6};
+//     // reversArray(rArray, size);
+//     // for(int i=0; i<size; i++){
+//     //     cout<<rArray[i];
+//     // }cout<<endl;
+
+//     // // int n=5;
+//     // int marks[n];
+
+
+
+//     // cout<<marks[0]<<"\n";
+//     // cout<<marks[2]<<"\n";
+//     // cout<<marks[3]<<"\n";
+//     // cout<<marks[4]<<"\n";
+//     // cout<<marks[1]<<"\n";
+   
+// // for(int i=0; i<n; i++){
+// //     cin>>marks[i];
+// // }
+
+// //     for(int i=0; i<n;i++){
+// //         cout<<marks[i]<<"\n";
+// //     }
+
+// // int n=5;
+// // int arr[n];
  
-for(int i=0; i<n;i++){
-    cin>>arr[i];
-}
+// // for(int i=0; i<n;i++){
+// //     cin>>arr[i];
+// // }
 
-int min=INT_MAX;
-int max=INT_MIN;
-int index;
-for(int i =0; i<n; i++){
-    if(arr[i]<min){
-        min= arr[i];
-        index=arr[min];
+// // int min=INT_MAX;
+// // int max=INT_MIN;
+// // int index;
+// // for(int i =0; i<n; i++){
+// //     if(arr[i]<min){
+// //         min= arr[i];
+// //         index=arr[min];
         
-    }
+// //     }
 
-}
-    cout<<"min number is :"<<min<<"\n";
-       cout<<"min number is :"<<index<<"\n";
-    for(int i =0; i<n; i++){
-    if(arr[i]>max){
-        max= arr[i];
-    }
-}
-    cout<<"max number is :"<<max<<"\n";
+// // }
+// //     cout<<"min number is :"<<min<<"\n";
+// //     for(int i =0; i<n; i++){
+// //     if(arr[i]>max){
+// //         max= arr[i];
+// //     }
+// // }
+// //     cout<<"max number is :"<<max<<"\n";
 
-    return 0; 
-}
+// //   int target = 3;
+// //   int n=5;
+// //   int ar[n]={2,3,44,63,3};
+
+// // for(int i = 0; i<n; i++){
+// //     if(ar[i]==target){
+// //         cout<<i <<endl;
+// //     }
+    
+// // }
+
+    
+
+//     return 0; 
+// }
